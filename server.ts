85287bb5b7fccb901022dca99add03f56f45786b
Purevs <59160227+Purevs@users.noreply.github.com>
2026-03-20 00:18:01 +0800

    Update server.ts

--- server.ts
+++ server.ts
@@ -436,11 +436,12 @@ startServer();
     }
   });
 
-  const PORT = Number(process.env.PORT) || 3000;
 
- app.listen(PORT, "0.0.0.0", () => {
- console.log(`Server running on port ${PORT}`);
- });
+
+app.listen(Number(process.env.PORT) || 3000, "0.0.0.0", () => {
+ console.log(`Server running on port ${Number(process.env.PORT) || 3000}`);
+});
+
 }
 
 startServer();
